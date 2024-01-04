8aa321bed3fe13fced53397cf2ce237d9252014d
ananthmgk <ananthmgk4002@gmail.com>
2024-01-04 22:19:43 +0530

    updated

--- Day_01/files.js
+++ Day_01/files.js
@@ -64,28 +64,6 @@ const Body = () => {
         >
           Search
         </button>
-        <div className="dropdown justify-items-center items-center relative inline-block">
-          <button
-            onClick={""}
-            className="dropbtn p-2 m-2 px-8 mx-10 bg-yellow-700 hover:block hover:bg-yellow-500 text-white rounded-md"
-          >
-            Category
-          </button>
-          <div className="dropdown-content absolute hidden bg-amber-100 min-w-40 shadow-lg">
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 1a
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 1b
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 1c
-            </Link>
-            <Link to="/" className="block text-black px-3 py-4">
-              Level 1d
-            </Link>
-          </div>
-        </div>
       </div>
       <div className="flex flex-wrap ">
         {filteredProducts.length == 0 ? (
