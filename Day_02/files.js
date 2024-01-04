4444c7dd22d573dacefeffdfc112ea2364b6e55e
ananthmgk <ananthmgk4002@gmail.com>
2024-01-04 21:49:42 +0530

    Created a repo for 100-days-of-code

---
+++ Day_02/files.js
@@ -0,0 +1,22 @@
+<div className="dropdown justify-items-center items-center relative inline-block">
+  <button
+    onClick={""}
+    className="dropbtn p-2 m-2 px-8 mx-10 bg-yellow-700 hover:block hover:bg-yellow-500 text-white rounded-md"
+  >
+    Category
+  </button>
+  <div className="dropdown-content absolute hidden bg-amber-100 min-w-40 shadow-lg">
+    <Link to="/" className="block text-black px-3 py-4">
+      Level 1a
+    </Link>
+    <Link to="/" className="block text-black px-3 py-4">
+      Level 1b
+    </Link>
+    <Link to="/" className="block text-black px-3 py-4">
+      Level 1c
+    </Link>
+    <Link to="/" className="block text-black px-3 py-4">
+      Level 1d
+    </Link>
+  </div>
+</div>;
---
+++ log.md
@@ -0,0 +1,3 @@
+Day 01 (02-01-2024) - I am creating a e commerce website name "My Basket", today i have inserted Debouncing method in my project.
+
+Day 02 (03-01-2024) - Today I have created a category tree button using tailwind css.
